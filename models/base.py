0f02206795ad8b5c0dcd6feb48a6274f43bda2c7
Lukas Jendele <lukas.jendele@gmail.com>
2018-02-25 22:18:01 +0100

    GAN model using graph TF, still needs to be finished.

--- models/base.py
+++ models/base.py
@@ -26,6 +26,7 @@ import tensorflow.contrib.eager as tfe
 
 class BaseModel(tfe.Network):
     """Base Network."""
+
     def __init__(self, name=''):
         super(BaseModel, self).__init__(name=name)
 
@@ -38,7 +39,8 @@ class BaseModel(tfe.Network):
         loss_value = self.loss(output_layer, labels)
         tf.contrib.summary.scalar('loss', loss_value)
         # TODO: Enable more metrics.
-        tf.contrib.summary.scalar('accuracy', tf.contrib.metrics.accuracy(predictions, labels))
+        tf.contrib.summary.scalar(
+            'accuracy', tf.contrib.metrics.accuracy(predictions, labels))
         #tf.contrib.summary.scalar('precision', tf.contrib.metrics.precision(predictions, labels))
         #tf.contrib.summary.scalar('recall', tf.contrib.metrics.recall(predictions, labels))
         return loss_value
@@ -53,10 +55,13 @@ class BaseModel(tfe.Network):
 
         for batch, (images, labels) in enumerate(tfe.Iterator(dataset)):
             with tf.contrib.summary.record_summaries_every_n_global_steps(10):
-                batch_model_loss = functools.partial(self.model_loss, labels, images)
-                optimizer.minimize(batch_model_loss, global_step=tf.train.get_global_step())
+                batch_model_loss = functools.partial(
+                    self.model_loss, labels, images)
+                optimizer.minimize(
+                    batch_model_loss, global_step=tf.train.get_global_step())
                 if log_interval and batch % log_interval == 0:
-                    print('Batch #%d\tLoss: %.6f' % (batch, batch_model_loss()))
+                    print('Batch #%d\tLoss: %.6f' %
+                          (batch, batch_model_loss()))
 
     def test(self, dataset):
         """Perform an evaluation of `model` on the examples from `dataset`."""
--- resources/flags.py
+++ resources/flags.py
@@ -9,29 +9,33 @@ FLAGS = tf.app.flags.FLAGS
 def define_flags():
     # Directories
     tf.app.flags.DEFINE_string('in_data_dir', 'data_in',
-                        "Directory from which to read input datasets.")
+                               "Directory from which to read input datasets.")
     tf.app.flags.DEFINE_string('out_data_dir', 'data_out',
-                           """Directory where to write event logs """
-                           """and checkpoint.""")
+                               """Directory where to write event logs """
+                               """and checkpoint.""")
     if os.name == 'nt':
         tf.app.flags.DEFINE_string('checkpoint_dir', 'e:/temp/tensorflow/checkpoints/',
-                        'Directory to save checkpoints in (once per epoch)')
+                                   'Directory to save checkpoints in (once per epoch)')
     else:
         tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/tensorflow/checkpoints/',
-                        'Directory to save checkpoints in (once per epoch)')    
+                                   'Directory to save checkpoints in (once per epoch)')
 
     # TF parameters
-    tf.app.flags.DEFINE_boolean("no_gpu", False, 'Disables GPU usage even if a GPU is available')
-    tf.app.flags.DEFINE_integer("log_interval", 100, 'How many batches to wait before logging training statistics.')
+    tf.app.flags.DEFINE_boolean(
+        "no_gpu", False, 'Disables GPU usage even if a GPU is available')
+    tf.app.flags.DEFINE_integer(
+        "log_interval", 100, 'How many batches to wait before logging training statistics.')
 
     # Optimization parameters
-    tf.app.flags.DEFINE_integer('epochs', 10, 'Training epoch count')
-    tf.app.flags.DEFINE_float('learning_rate', 0.01, 'Learning rate')
+    tf.app.flags.DEFINE_integer('epochs', 50, 'Training epoch count')
+    tf.app.flags.DEFINE_float('learning_rate', 0.0002, 'Learning rate')
     tf.app.flags.DEFINE_float('momentum', 0.5, 'SGD momentum')
-    tf.app.flags.DEFINE_integer('batch_size', 256, 'Training batch size')
+    tf.app.flags.DEFINE_integer('batch_size', 100, 'Training batch size')
 
     # Model parameters
-    tf.app.flags.DEFINE_integer('conv_size', 32, 'First convolution layer size')
-    
+    tf.app.flags.DEFINE_integer(
+        'conv_size', 32, 'First convolution layer size')
+
     # Jupyter notebook params
-    tf.app.flags.DEFINE_string('f', 'kernel', 'Kernel')  # Only to avoid raising UnrecognizedFlagError
+    # Only to avoid raising UnrecognizedFlagError
+    tf.app.flags.DEFINE_string('f', 'kernel', 'Kernel')
