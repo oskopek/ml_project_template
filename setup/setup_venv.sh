b96a2b8f8cc09712dd1171135663836d0fe2e7cf
Ondrej Skopek <skopekondrej@gmail.com>
2018-02-28 12:55:20 +0100

    Adding pre-commit hook + installation

--- setup/setup_venv.sh
+++ setup/setup_venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ln -s ../../setup/pre-commit.sh .git/hooks/pre-commit
 pip install jupyter_tensorboard==0.1.5
 jupyter contrib nbextension install --user
 jupyter nbextension enable code_prettify/code_prettify
