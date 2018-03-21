621864703553d94c15378b289c75bb0b4393dc80
Ondrej Skopek <skopekondrej@gmail.com>
2018-03-21 23:19:12 +0100

    Fixed pre-commit script failing when formatting

--- setup/pre-commit.sh
+++ setup/pre-commit.sh
@@ -4,27 +4,27 @@ function scripttest {
     echo -ne "\nPython cleaning test... "
     stagedfiles=$(git diff --cached --name-only | grep '.py$')
     if [ -z "$stagedfiles" ]; then
-        echo -e "PASSED"
+        echo -e "SKIPPED (No staged Python files)"
         return 0
     fi
 
     unformatted=$(yapf --diff $stagedfiles | grep "(reformatted)" | awk '{print $2}')
     failed=$?
-    if [ -n "$failed" ]; then
-        echo -e "FAILED -- CRASHED"
-        return 1
-    fi
     if [ -z "$unformatted" ]; then
+        if [ -n "$failed" ]; then
+            echo -e "FAILED (Formatter crashed)"
+            return 1
+        fi
         echo -e "PASSED"
         return 0
     fi
 
-    echo "The following files are not formatted properly:"
+    echo -e "FAILED (Formatting)\n\n  The following files are not formatted properly:"
     for fn in $unformatted; do
         echo "    $fn"
     done
 
-    echo -e "\nPython files must be formatted with YAPF. Please run:"
+    echo -e "\n  Python files must be formatted with YAPF. Please run:"
     echo "    ./setup/clean.sh"
     return 1
 }
@@ -33,27 +33,27 @@ function notebooktest {
     echo -ne "\nNotebook cleaning test... "
     stagedfiles=$(git diff --cached --name-only | grep '.ipynb$')
     if [ -z "$stagedfiles" ]; then
-        echo -e "PASSED"
+        echo -e "SKIPPED (No staged notebooks)"
         return 0
     fi
 
     unformatted=$(python setup/yapf_nbformat.py --dry_run $stagedfiles | grep "(reformatted)" | awk '{print $2}')
     failed=$?
-    if [ -n "$failed" ]; then
-        echo -e "FAILED -- CRASHED"
-        return 1
-    fi
     if [ -z "$unformatted" ]; then
+        if [ -n "$failed" ]; then
+            echo -e "FAILED (Formatter crashed)"
+            return 1
+        fi
         echo -e "PASSED"
         return 0
     fi
 
-    echo "The following files are not formatted properly:"
+    echo -e "FAILED (Formatting)\n\n  The following files are not formatted properly:"
     for fn in $unformatted; do
         echo "    $fn"
     done
 
-    echo -e "\nNotebook files must be formatted with a custom nbformat-YAPF script. Please run:"
+    echo -e "\n  Notebook files must be formatted with a custom nbformat-YAPF script. Please run:"
     echo "    ./setup/clean.sh"
     return 1
 }
